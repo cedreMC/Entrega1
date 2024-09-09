40d7e02cae4bc41ea92f4779789d134fdf287e42
cedreMC <128849794+cedreMC@users.noreply.github.com>
2024-09-08 23:33:45 -0300

    Etapa 1: Aplicación Docentes

---
+++ si/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+
+interface Menu{
+  icon:string;
+  redirecTo: string;
+  name:string;
+}
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+
+  
+})
+export class AppComponent {
+
+  menu:Menu[]=[
+    {
+      icon:'exit-outline',
+      redirecTo: '/comienzo',
+      name:'Cerrar la sesión'
+    },
+    {
+      icon:'person-circle-outline',
+      redirecTo: '/perfil',
+      name:'Perfil'
+    },
+    
+  ]
+
+  constructor() {}
+}
